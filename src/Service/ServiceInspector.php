61cb94fe7b851786a1f9828914d4408eed7fefd3
Mathijs de Kruyf <m.d.kruyf@audax.nl>
2020-08-09 11:57:41 +0200

    Add ServiceInspectorCommand and ServiceInspector

---
+++ src/Service/ServiceInspector.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+final class ServiceInspector
+{
+    /** @var ContainerInterface */
+    private ContainerInterface $container;
+
+    /** @var LoggerInterface */
+    private LoggerInterface $logger;
+
+    /** @var string|null */
+    private ?string $yamlFilePath;
+
+    public function __construct(ContainerInterface $container, LoggerInterface $logger)
+    {
+        $this->container = $container;
+        $this->logger = $logger;
+        $this->yamlFilePath = null;
+    }
+
+    /**
+     * @param string $yamlFilePath
+     */
+    public function setYamlFilePath(string $yamlFilePath): void
+    {
+        $this->yamlFilePath = $yamlFilePath;
+    }
+
+    public function inspect(): void
+    {
+
+    }
+}
