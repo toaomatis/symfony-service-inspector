4832ede5f3821d09492039be8afd947d1ceb6a04
Mathijs de Kruyf <hpmdekruyf@gmail.com>
2020-08-09 20:29:51 +0200

    Add checks to ServiceInspector

--- src/Service/ServiceInspector.php
+++ src/Service/ServiceInspector.php
@@ -15,14 +15,22 @@ final class ServiceInspector
     /** @var LoggerInterface */
     private LoggerInterface $logger;
 
-    /** @var array */
+    /** @var string[] */
     private array $hashes;
+
+    /** @var int[] */
+    private array $references;
+
+    /** @var int */
+    private int $duplicateCounter;
 
     public function __construct(ContainerInterface $container, LoggerInterface $logger)
     {
         $this->container = $container;
         $this->logger = $logger;
         $this->hashes = [];
+        $this->references = [];
+        $this->duplicateCounter = 0;
     }
 
     /**
@@ -33,6 +41,10 @@ final class ServiceInspector
         $yamlFile = new YamlFile($yamlFilePath, $this->logger);
         $yamlFile->parse();
         $this->createHashmap($yamlFile);
+        $this->logger->info(sprintf('Found %d duplicates in "%s" recursively.', $this->duplicateCounter,
+            $yamlFilePath));
+        $this->countReferences($yamlFile);
+        $this->reportReferences();
     }
 
     private function createHashmap(YamlFile $yamlFile): void
@@ -42,11 +54,21 @@ final class ServiceInspector
             if ($name === '_defaults') {
                 continue;
             }
+            if (array_key_exists($name, $this->references) === false) {
+                $this->references[$name] = 0;
+            }
             $serialized = serialize($service);
             $hash = md5($serialized);
             if (array_key_exists($hash, $this->hashes) === true) {
                 $matchedName = $this->hashes[$hash];
-                $this->logger->warning(sprintf('Found duplicate hash for service "%s" --> "%s"', $name, $matchedName));
+                if ($matchedName !== $name) {
+                    $this->logger->warning(
+                        sprintf('Found duplicate hash for service "%s" --> "%s".', $name, $matchedName));
+                    $this->duplicateCounter++;
+
+                } else {
+                    $this->logger->debug(sprintf('Found duplicate hash and service "%s".', $name));
+                }
                 continue;
             }
             $this->hashes[$hash] = $name;
@@ -54,6 +76,87 @@ final class ServiceInspector
         $imports = $yamlFile->getImports();
         foreach ($imports as $import) {
             $this->createHashmap($import);
+        }
+    }
+
+    /**
+     * @param YamlFile $yamlFile
+     */
+    private function countReferences(YamlFile $yamlFile): void
+    {
+        $services = $yamlFile->getServices();
+        foreach ($services as $name => $service) {
+            if ($name === '_defaults') {
+                continue;
+            }
+            $factory = $service['factory'] ?? [];
+            if (is_array($factory) === false) {
+                $factories = explode(':', $factory);
+                $factory = ['@' . $factories[0]];
+            }
+
+            $this->examineReferences($service['arguments'] ?? []);
+            $this->examineReferences($service['calls'] ?? []);
+            $this->examineReferences($factory ?? []);
+            //$this->examineReferences($service['tags'] ?? []);
+        }
+        $imports = $yamlFile->getImports();
+        foreach ($imports as $import) {
+            $this->countReferences($import);
+        }
+    }
+
+    /**
+     * @param array $references
+     */
+    private function examineReferences(array $references): void
+    {
+        foreach ($references as $key => $reference) {
+            if (is_array($reference) === true) {
+                $this->examineReferences($reference);
+
+                continue;
+            }
+
+            $name = null;
+
+            $this->logger->debug(sprintf('Key "%s" Reference "%s"', $key, $reference));
+            if ((is_string($reference) === true) && (empty($reference) === false) && ($reference[0] === '@')) {
+                $name = substr($reference, 1);
+                $this->addReference($name);
+            } elseif ((is_string($key) === true) && (empty($key) === false) && (strpos($key, '\\') !== false)) {
+                $name = $reference;
+                $this->addReference($name);
+            } elseif ((is_string($reference) === true) && (empty($reference) === false) &&
+                (strpos($reference, '.') !== false)) {
+                $name = $reference;
+                $this->addReference($name, false);
+            }
+
+        }
+    }
+
+    private function addReference(string $name, bool $forceAdd = true): void
+    {
+        if (array_key_exists($name, $this->references) === false) {
+            if ($forceAdd === true) {
+                $this->logger->warning(sprintf('Found unreferenced "%s".', $name));
+                $this->references[$name] = 1;
+            } else {
+                $this->logger->debug(sprintf('Skipping unreferenced "%s".', $name));
+            }
+        } else {
+            $this->references[$name] += 1;
+        }
+    }
+
+    private function reportReferences(): void
+    {
+        foreach ($this->references as $name => $count) {
+            $this->logger->debug(sprintf('Service "%s" is used %d times', $name, $count));
+            if ($count === 0) {
+                $this->logger->warning(sprintf('Unreferenced Service "%s"', $name));
+            }
         }
     }
 }
